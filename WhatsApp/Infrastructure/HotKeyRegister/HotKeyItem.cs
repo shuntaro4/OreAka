2d80d8c7729aca080ac6cf0bbf9a5aef008946b0
shuntaro4 <shuntaro4@gmail.com>
2019-09-08 20:49:14 +0900

    add HotKeyItem class.

---
+++ WhatsApp/Infrastructure/HotKeyRegister/HotKeyItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WhatsApp.Infrastructure.HotKeyRegister
+{
+    public class HotKeyItem
+    {
+        public ModifierKeys ModifierKeys { get; }
+
+        public Key Key { get; }
+
+        public EventHandler EventHandler { get; }
+
+        public HotKeyItem(ModifierKeys modifierKeys, Key key, EventHandler eventHandler)
+        {
+            ModifierKeys = modifierKeys;
+            Key = key;
+            EventHandler = eventHandler;
+        }
+    }
+}
