31211ec7fbe7a02a77a745d23112a720a44270bb
shuntaro4 <shuntaro4@gmail.com>
2019-09-08 23:24:30 +0900

    add ContentRenderd event.

--- WhatsApp/MainWindow.xaml.cs
+++ WhatsApp/MainWindow.xaml.cs
@@ -1,12 +1,26 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using WhatsApp.Infrastructure.HotKeyRegister;
 
 namespace WhatsApp
 {
     public partial class MainWindow : Window
     {
+        private IHotKeyRegister hotKeyRegister;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            hotKeyRegister = new HotKeyRegister(this);
+            hotKeyRegister.RegistKey(ModifierKeys.Control | ModifierKeys.Shift, Key.Space, (_, __) =>
+            {
+                // todo
+            });
         }
     }
 }
