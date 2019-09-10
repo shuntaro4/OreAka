dedfe7b484787f85952a91b314d60a51755c4052
shuntaro4 <shuntaro4@gmail.com>
2019-09-10 22:29:04 +0900

    add WorkTask class.

---
+++ WhatsApp.WPF/Domain/WorkTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhatsApp.WPF.Domain
+{
+    public class WorkTask
+    {
+        public string Title { get; }
+
+        public int Minutes { get; }
+
+        public DateTime CreratedAt { get; } = DateTime.Now;
+
+        public WorkTask(string answer)
+        {
+            // todo parse string
+        }
+    }
+}
