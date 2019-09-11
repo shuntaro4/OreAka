d02a43e7be3d98cc08f4d1c9ba9e9529e5d00778
shuntaro4 <shuntaro4@gmail.com>
2019-09-11 22:42:42 +0900

    add IWorkTaskService interface.

---
+++ WhatsApp.WPF/ApplicationService/IWorkTaskService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WhatsApp.WPF.ApplicationService
+{
+    public interface IWorkTaskService
+    {
+        Task<bool> RegistWorkTask(string inputString);
+    }
+}
