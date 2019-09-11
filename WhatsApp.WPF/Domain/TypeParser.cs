8e1b01929aea493f02cab6f01c5206c020469fec
shuntaro4 <shuntaro4@gmail.com>
2019-09-11 22:36:38 +0900

    add TypeParser class.

---
+++ WhatsApp.WPF/Domain/TypeParser.cs
@@ -0,0 +1,14 @@
+﻿namespace WhatsApp.WPF.Domain
+{
+    public class TypeParser
+    {
+        public static int ToInt(string target)
+        {
+            if (int.TryParse(target, out var result))
+            {
+                return result;
+            }
+            return 0;
+        }
+    }
+}
