4990dfc7247bca87c52bb7f3ec1ce101ad04586c
shuntaro4 <shuntaro4@gmail.com>
2019-09-12 23:05:08 +0900

    add IWorkTaskRepository interface.

---
+++ WhatsApp.WPF/Infrastructure/Repositories/IWorkTaskRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WhatsApp.WPF.Domain;
+
+namespace WhatsApp.WPF.Infrastructure.Repositories
+{
+    public interface IWorkTaskRepository
+    {
+        Task Save(WorkTask workTask);
+    }
+}
