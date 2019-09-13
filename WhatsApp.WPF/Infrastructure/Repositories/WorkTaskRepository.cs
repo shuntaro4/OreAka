09905a5792401bfc60ab0752d93598d74d477667
shuntaro4 <shuntaro4@gmail.com>
2019-09-13 21:55:15 +0900

    add WorkTaskRepository class.

---
+++ WhatsApp.WPF/Infrastructure/Repositories/WorkTaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using WhatsApp.WPF.Domain;
+
+namespace WhatsApp.WPF.Infrastructure.Repositories
+{
+    public class WorkTaskRepository : IWorkTaskRepository
+    {
+        public Task Save(WorkTask workTask)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
