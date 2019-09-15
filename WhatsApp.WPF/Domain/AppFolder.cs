467d28af0f127bd6a6a7135b51bc9aa3982bc96f
shuntaro4 <shuntaro4@gmail.com>
2019-09-15 18:15:25 +0900

    add AppFolder class.

---
+++ WhatsApp.WPF/Domain/AppFolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace WhatsApp.WPF.Domain
+{
+    public class AppFolder
+    {
+        public static string CreateOutputFolder()
+        {
+            var outputFolderPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "WhatsApp");
+
+            if (Directory.Exists(outputFolderPath))
+            {
+                return outputFolderPath;
+            }
+
+            var result = Directory.CreateDirectory(outputFolderPath);
+            return result.FullName;
+        }
+    }
+}
