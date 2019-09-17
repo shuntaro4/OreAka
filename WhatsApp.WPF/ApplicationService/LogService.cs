850a40d748cb0e0d9ebdceedfc495625d187831a
shuntaro4 <shuntaro4@gmail.com>
2019-09-17 22:30:03 +0900

    add LogService class.

---
+++ WhatsApp.WPF/ApplicationService/LogService.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+
+namespace WhatsApp.WPF.ApplicationService
+{
+    public class LogService : ILogService
+    {
+        private ILogger logger;
+
+        public LogService()
+        {
+            // todo : change output folder.
+            logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+    }
+}
