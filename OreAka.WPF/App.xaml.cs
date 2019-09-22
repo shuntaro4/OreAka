88031e24a918d36116caf435033e0ddf5585d008
shuntaro4 <shuntaro4@gmail.com>
2019-09-23 02:28:34 +0900

    add classes to DI container.

--- OreAka.WPF/App.xaml.cs
+++ OreAka.WPF/App.xaml.cs
@@ -1,8 +1,8 @@
-﻿using Prism.Ioc;
-using System.Windows;
-using OreAka.WPF.ApplicationService;
+﻿using OreAka.WPF.ApplicationService;
 using OreAka.WPF.Infrastructure.Repositories;
 using OreAka.WPF.Presentation.Views;
+using Prism.Ioc;
+using System.Windows;
 
 namespace OreAka.WPF
 {
@@ -16,7 +16,9 @@ namespace OreAka.WPF
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.Register<IWorkTaskService, WorkTaskService>();
+            containerRegistry.Register<IPreferencesService, PreferencesService>();
             containerRegistry.Register<IWorkTaskRepository, WorkTaskRepository>();
+            containerRegistry.Register<IPreferencesRepository, PreferencesRepository>();
             containerRegistry.Register<ILogService, LogService>();
             containerRegistry.Register<IJsonSerializer, JsonSerializer>();
         }
