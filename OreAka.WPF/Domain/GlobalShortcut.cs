72faf3ea9fc051b3da063ea3d0f628dfaea365fe
shuntaro4 <shuntaro4@gmail.com>
2019-09-23 02:22:45 +0900

    add GlobalShortcut class.

---
+++ OreAka.WPF/Domain/GlobalShortcut.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+using System.Windows.Input;
+
+namespace OreAka.WPF.Domain
+{
+    [DataContract]
+    public class GlobalShortcut
+    {
+        [DataMember]
+        public ModifierKeys ModifierKeys { get; private set; }
+
+        [DataMember]
+        public Key Key { get; private set; }
+
+        public GlobalShortcut(ModifierKeys modifierKeys, Key key)
+        {
+            ModifierKeys = modifierKeys;
+            Key = key;
+        }
+    }
+}
