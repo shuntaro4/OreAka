24bd9c0cd7fcaf491f69c4706ba0ae0fce2bdcef
shuntaro4 <shuntaro4@gmail.com>
2019-09-24 00:55:44 +0900

    change Version property to fixed value.

--- OreAka.WPF/Domain/Preferences.cs
+++ OreAka.WPF/Domain/Preferences.cs
@@ -6,6 +6,8 @@ namespace OreAka.WPF.Domain
     [DataContract]
     public class Preferences
     {
+        private readonly int nowVersion = 1;
+
         [DataMember]
         public int Version { get; private set; }
 
@@ -15,16 +17,16 @@ namespace OreAka.WPF.Domain
         [DataMember]
         public GlobalShortcut ShowHideShortcut { get; private set; }
 
-        public Preferences(int version, string delimiter, GlobalShortcut showHideShorcut)
+        public Preferences(string delimiter, GlobalShortcut showHideShorcut)
         {
-            Version = version;
+            Version = nowVersion;
             Delimiter = delimiter;
             ShowHideShortcut = showHideShorcut;
         }
 
         public static Preferences GenerateDefault()
         {
-            var result = new Preferences(1, ",", new GlobalShortcut(ModifierKeys.Control | ModifierKeys.Shift, Key.Space));
+            var result = new Preferences(",", new GlobalShortcut(ModifierKeys.Control | ModifierKeys.Shift, Key.Space));
             return result;
         }
     }
