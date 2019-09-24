4545d8d64a040d2a5a6c5c39b75318c3b9816a02
shuntaro4 <shuntaro4@gmail.com>
2019-09-25 00:06:23 +0900

    add JsonSerializer Test.

---
+++ OreAka.WPF.Test/Infrastructure/Repositories/JsonSerializerTest.cs
@@ -0,0 +1,32 @@
+﻿using OreAka.WPF.Domain;
+using Xunit;
+
+namespace OreAka.WPF.Infrastructure.Repositories
+{
+    public class JsonSerializerTest
+    {
+        private JsonSerializer target = new JsonSerializer();
+
+        [Fact(DisplayName = "正：Json文字列に正しくシリアライズできる")]
+        [Trait("JsonSerializer", "Serialize")]
+        public void SerializeTrue1()
+        {
+            var model = Preferences.GenerateDefault();
+            var actual = target.Serialize(model);
+            Assert.Equal(@"{""Delimiter"":"","",""ShowHideShortcut"":{""Key"":18,""ModifierKeys"":6},""Version"":1}", actual);
+        }
+
+        [Fact(DisplayName = "正：objectに正しくデシリアライズできる")]
+        [Trait("JsonSerializer", "Desirialize")]
+        public void DesirializeTrue1()
+        {
+            var expected = Preferences.GenerateDefault();
+            var json = @"{""Delimiter"":"","",""ShowHideShortcut"":{""Key"":18,""ModifierKeys"":6},""Version"":1}";
+            var actual = target.Desirialize<Preferences>(json);
+            Assert.Equal(expected.Version, actual.Version);
+            Assert.Equal(expected.Delimiter, actual.Delimiter);
+            Assert.Equal(expected.ShowHideShortcut.ModifierKeys, actual.ShowHideShortcut.ModifierKeys);
+            Assert.Equal(expected.ShowHideShortcut.Key, actual.ShowHideShortcut.Key);
+        }
+    }
+}
