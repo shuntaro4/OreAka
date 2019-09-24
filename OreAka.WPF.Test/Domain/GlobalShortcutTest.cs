5d3e0df7a2197abd01a0c0dfeb568e87f4579afe
shuntaro4 <shuntaro4@gmail.com>
2019-09-25 00:06:00 +0900

    add GlobalShortcut Test.

---
+++ OreAka.WPF.Test/Domain/GlobalShortcutTest.cs
@@ -0,0 +1,19 @@
+﻿using OreAka.WPF.Domain;
+using Xunit;
+
+namespace OreAka.WPF.Test.Domain
+{
+    public class GlobalShortcutTest
+    {
+        [Fact(DisplayName = "正：コンストラクタ")]
+        [Trait("GrobalShortcut", "GrobalShortcut")]
+        public void GlobalShortcutTrue1()
+        {
+            var actual = new GlobalShortcut(System.Windows.Input.ModifierKeys.Alt, System.Windows.Input.Key.A);
+
+            Assert.NotNull(actual);
+            Assert.Equal(System.Windows.Input.ModifierKeys.Alt, actual.ModifierKeys);
+            Assert.Equal(System.Windows.Input.Key.A, actual.Key);
+        }
+    }
+}
