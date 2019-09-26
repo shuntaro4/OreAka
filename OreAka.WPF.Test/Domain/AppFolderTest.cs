28d5c9e31b29e26373d06d1303a91759fb656c96
shuntaro4 <shuntaro4@gmail.com>
2019-09-26 22:54:19 +0900

    fix AppFolder.CreateOutputFolder Tests.

--- OreAka.WPF.Test/Domain/AppFolderTest.cs
+++ OreAka.WPF.Test/Domain/AppFolderTest.cs
@@ -1,42 +1,45 @@
-﻿using System;
+﻿using OreAka.WPF.Domain;
+using System;
 using System.IO;
-using OreAka.WPF.Domain;
 using Xunit;
 
 namespace OreAka.WPF.Test.Domain
 {
-    public class AppFolderTest
+    public class AppFolderTest : IDisposable
     {
+        private AppFolder target = new AppFolder("../AppFolder");
+
+        public void Dispose()
+        {
+            if (Directory.Exists(target.OutputFolder))
+            {
+                Directory.Delete(target.OutputFolder);
+            }
+        }
+
         [Fact(DisplayName = "正：フォルダが存在しない場合、フォルダが作成される")]
         [Trait("AppFolder", "CreateOutputFolder")]
         public void CreateOutputFolderTrue1()
         {
-            var expected = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "OreAka");
-            if (Directory.Exists(expected))
-            {
-                Directory.Delete(expected, true);
-            }
-            Assert.False(Directory.Exists(expected));
+            var outputFolder = target.OutputFolder;
 
-            var actual = AppFolder.CreateOutputFolder();
-            Assert.Equal(expected, actual);
-            Assert.True(Directory.Exists(actual));
+            Assert.False(Directory.Exists(outputFolder));
+            target.CreateOutputFolder();
+            Assert.True(Directory.Exists(outputFolder));
         }
 
         [Fact(DisplayName = "正：フォルダが存在する場合、何もしない")]
         [Trait("AppFolder", "CreateOutputFolder")]
         public void CreateOutputFolderTrue2()
         {
-            var expected = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "OreAka");
-            if (!Directory.Exists(expected))
-            {
-                Directory.CreateDirectory(expected);
-            }
-            Assert.True(Directory.Exists(expected));
+            var outputFolder = target.OutputFolder;
 
-            var actual = AppFolder.CreateOutputFolder();
-            Assert.Equal(expected, actual);
-            Assert.True(Directory.Exists(actual));
+            Assert.False(Directory.Exists(outputFolder));
+            target.CreateOutputFolder();
+
+            Assert.True(Directory.Exists(outputFolder));
+            target.CreateOutputFolder();
+            Assert.True(Directory.Exists(outputFolder));
         }
     }
 }
