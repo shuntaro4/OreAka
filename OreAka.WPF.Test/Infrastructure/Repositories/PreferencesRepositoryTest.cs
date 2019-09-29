b27b9bacef795a541b5969a2c7cecafe516a74fc
shuntaro4 <shuntaro4@gmail.com>
2019-09-29 20:02:18 +0900

    add PreferencesRepository.Exists Tests.

---
+++ OreAka.WPF.Test/Infrastructure/Repositories/PreferencesRepositoryTest.cs
@@ -0,0 +1,49 @@
+﻿using OreAka.WPF.Infrastructure.Repositories;
+using System.IO;
+using Xunit;
+
+namespace OreAka.WPF.Test.Infrastructure.Repositories
+{
+    public class PreferencesRepositoryTest
+    {
+        private JsonSerializer jsonSerializer = new JsonSerializer();
+
+        [Fact(DisplayName = "正：jsonファイルが存在する場合 true")]
+        [Trait("PreferencesRepository", "Exists")]
+        public void ExistsTrue1()
+        {
+            var path = "ExistsTrue1.json";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            var target = new PreferencesRepository(path)
+            {
+                JsonSerializer = jsonSerializer
+            };
+            var actual = target.Exists();
+            Assert.True(actual);
+        }
+
+        [Fact(DisplayName = "正：jsonファイルが存在しない場合 false")]
+        [Trait("PreferencesRepository", "Exists")]
+        public void ExistsTrue2()
+        {
+            var path = "ExistsTrue2.json";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            var target = new PreferencesRepository(path)
+            {
+                JsonSerializer = jsonSerializer
+            };
+            var actual = target.Exists();
+            Assert.False(actual);
+        }
+    }
+}
