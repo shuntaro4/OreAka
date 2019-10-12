45b2722225bf869bcac0445a0347105d57a1309d
shuntaro4 <shuntaro4@gmail.com>
2019-10-13 01:06:31 +0900

    add AboutWindow.

---
+++ OreAka.WPF/Presentation/ViewModels/AboutWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Reflection;
+
+namespace OreAka.WPF.Presentation.ViewModels
+{
+    public class AboutWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>("OreAka - About");
+
+        public ReactiveProperty<string> Version { get; } = new ReactiveProperty<string>("");
+
+        public AboutWindowViewModel()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Version.Value = $"Version {assembly.GetName().Version}";
+        }
+    }
+}
---
+++ OreAka.WPF/Presentation/Views/AboutWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OreAka.WPF.Presentation.Views
+{
+    public partial class AboutWindow : WindowEx
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void DragMove(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var url = e.Uri.AbsoluteUri;
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            e.Handled = true;
+        }
+    }
+}
