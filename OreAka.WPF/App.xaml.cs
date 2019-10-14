981a6ea9963faa3cce62dc709326d8390ac996d8
shuntaro4 <shuntaro4@gmail.com>
2019-10-14 12:48:36 +0900

    Prohibit multiple launching of apps. #6

--- OreAka.WPF/App.xaml.cs
+++ OreAka.WPF/App.xaml.cs
@@ -4,12 +4,19 @@ using OreAka.WPF.Infrastructure.Repositories;
 using OreAka.WPF.Presentation.Views;
 using Prism.Ioc;
 using System;
+using System.Threading;
 using System.Windows;
 
 namespace OreAka.WPF
 {
     public partial class App
     {
+        private static readonly string mutexName = "OreAka";
+
+        private static Mutex mutex = new Mutex(false, mutexName);
+
+        private static bool hasHandle = false;
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
@@ -24,6 +31,29 @@ namespace OreAka.WPF
             containerRegistry.Register<ILogService, LogService>();
             containerRegistry.Register<IJsonSerializer, JsonSerializer>();
             containerRegistry.RegisterInstance(typeof(AppFolder), new AppFolder(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)));
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            hasHandle = mutex.WaitOne(0, false);
+            if (!hasHandle)
+            {
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            if (hasHandle)
+            {
+                mutex.ReleaseMutex();
+            }
+            mutex.Close();
         }
     }
 }
