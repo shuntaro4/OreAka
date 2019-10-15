9b74176e8da89285d9fbaff1cec279bf6330e556
shuntaro4 <shuntaro4@gmail.com>
2019-10-16 00:31:45 +0900

    add a border to the window.

--- OreAka.WPF/Presentation/Views/WindowEx.cs
+++ OreAka.WPF/Presentation/Views/WindowEx.cs
@@ -1,14 +1,18 @@
-﻿using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Windows;
 
 namespace OreAka.WPF.Presentation.Views
 {
-    public class WindowEx : Window
+    public class WindowEx : MetroWindow
     {
         public WindowEx()
         {
             WindowStyle = WindowStyle.None;
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             AllowsTransparency = true;
+            ResizeMode = ResizeMode.NoResize;
+            TitleBarHeight = 0;
+            WindowTransitionsEnabled = false;
         }
     }
 }
