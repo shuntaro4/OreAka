418bba3c79c01a34fdf2ea37442e4a88b45336a4
shuntaro4 <shuntaro4@gmail.com>
2019-10-18 00:00:55 +0900

    add AppTheme.IsLightTheme Tests.

--- OreAka.WPF.Test/Domain/AppThemeTest.cs
+++ OreAka.WPF.Test/Domain/AppThemeTest.cs
@@ -5,6 +5,7 @@ namespace OreAka.WPF.Test.Domain
 {
     public class AppThemeTest
     {
+        #region IsDarkTheme
         [Fact(DisplayName = "正：Theme=Dark.Steelの場合、True")]
         [Trait("AppTheme", "IsDarkTheme")]
         public void IsDarkThemeTrue1()
@@ -31,5 +32,35 @@ namespace OreAka.WPF.Test.Domain
             var actual = target.IsDarkTheme;
             Assert.False(actual);
         }
+        #endregion
+
+        #region IsLightTheme
+        [Fact(DisplayName = "正：Theme=Dark.Steelの場合、False")]
+        [Trait("AppTheme", "IsLightTheme")]
+        public void IsLightThemeTrue1()
+        {
+            var target = AppTheme.GenerateDarkTheme();
+            var actual = target.IsLightTheme;
+            Assert.False(actual);
+        }
+
+        [Fact(DisplayName = "正：Theme=Light.Steelの場合、True")]
+        [Trait("AppTheme", "IsLightTheme")]
+        public void IsLightThemeTrue2()
+        {
+            var target = AppTheme.GenerateLightTheme();
+            var actual = target.IsLightTheme;
+            Assert.True(actual);
+        }
+
+        [Fact(DisplayName = "正：Theme=hogeの場合、False")]
+        [Trait("AppTheme", "IsLightTheme")]
+        public void IsLightThemeFalse1()
+        {
+            var target = AppTheme.GenerateNew("hoge");
+            var actual = target.IsLightTheme;
+            Assert.False(actual);
+        }
+        #endregion
     }
 }
