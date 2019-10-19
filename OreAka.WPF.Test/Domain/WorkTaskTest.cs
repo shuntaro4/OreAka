882cefe21cd6902551944ee3c326ba2e2ca3083d
shuntaro4 <shuntaro4@gmail.com>
2019-10-19 18:41:03 +0900

    fix WorkTask Tests.

--- OreAka.WPF.Test/Domain/WorkTaskTest.cs
+++ OreAka.WPF.Test/Domain/WorkTaskTest.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using OreAka.WPF.Domain;
+﻿using OreAka.WPF.Domain;
+using System;
 using Xunit;
 
 namespace OreAka.WPF.Test.Domain
@@ -68,11 +68,11 @@ namespace OreAka.WPF.Test.Domain
         public void WorkTaskFalse4()
         {
             var answer = "C# 80";
-            var actual = new WorkTask(answer);
-
-            Assert.NotNull(actual);
-            Assert.Equal("C# 80", actual.Title);
-            Assert.Equal(0, actual.Minutes);
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                var actual = new WorkTask(answer);
+            });
+            Assert.Equal("Answer format is invalid.(e.g \"send mail, 90\")", exception.Message);
         }
 
         [Fact(DisplayName = "異：Answer = カンマ後が数値文字列でない")]
@@ -80,11 +80,11 @@ namespace OreAka.WPF.Test.Domain
         public void WorkTaskFalse5()
         {
             var answer = "C#,hoge";
-            var actual = new WorkTask(answer);
-
-            Assert.NotNull(actual);
-            Assert.Equal("C#", actual.Title);
-            Assert.Equal(0, actual.Minutes);
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                var actual = new WorkTask(answer);
+            });
+            Assert.Equal("Answer format is invalid.(e.g \"send mail, 90\")", exception.Message);
         }
     }
 }
